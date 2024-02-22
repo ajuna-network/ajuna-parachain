7a18239195fc272794ed0b5e05def7585116d01b
Dídac Sementé Fernández <didac@ajuna.io>
2024-02-22 09:31:59 +0100

    * Bump version to 0.2.3

--- runtime/ajuna/src/lib.rs
+++ runtime/ajuna/src/lib.rs
@@ -216,7 +216,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("ajuna"),
 	impl_name: create_runtime_str!("ajuna"),
 	authoring_version: 1,
-	spec_version: 202,
+	spec_version: 203,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
