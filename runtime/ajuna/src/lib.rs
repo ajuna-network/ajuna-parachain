3b32015f28905477c7d8b125191e7361d66b685c
clangenb <37865735+clangenb@users.noreply.github.com>
2024-06-06 14:39:41 +0200

    add missing pallet-collator-selection migration (#35)

--- runtime/ajuna/src/lib.rs
+++ runtime/ajuna/src/lib.rs
@@ -154,9 +154,7 @@ pub type Executive = frame_executive::Executive<
 	Migrations,
 >;
 
-type Migrations = ();
-
-//type Migrations = (pallet_ajuna_awesome_avatars::migration::v6::MigrateToV6<Runtime>,);
+type Migrations = (pallet_collator_selection::migration::v2::MigrationToV2<Runtime>,);
 
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
 /// node's balance type.
