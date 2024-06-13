a865748aaeca32fc9555debce50b11de19541485
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-06-13 11:22:29 +0200

    [runtime] remove obsolete migrations

--- runtime/ajuna/src/lib.rs
+++ runtime/ajuna/src/lib.rs
@@ -150,7 +150,7 @@ pub type Executive = frame_executive::Executive<
 	Migrations,
 >;
 
-type Migrations = (pallet_collator_selection::migration::v2::MigrationToV2<Runtime>,);
+type Migrations = ();
 
 /// Handles converting a weight scalar to a fee value, based on the scale and granularity of the
 /// node's balance type.
