2d473e3f849de3d76f095aaa8be151090f8d426a
Christian Langenbacher <clangenb+gh@protonmail.ch>
2024-07-10 14:42:33 +0200

    add `CheckMetadataHash` signed extension

--- node/build.rs
+++ node/build.rs
@@ -1,22 +1,30 @@
-// Copyright (C) Parity Technologies (UK) Ltd.
-// This file is part of Cumulus.
+// Ajuna Node
+// Copyright (C) 2022 BlogaTech AG
 
-// Substrate is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
-// Substrate is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
 
-// You should have received a copy of the GNU General Public License
-// along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
-
+#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
-	generate_cargo_keys();
-	rerun_if_git_head_changed();
+	substrate_wasm_builder::WasmBuilder::build_using_defaults()
 }
+
+#[cfg(all(feature = "std", feature = "metadata-hash"))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::init_with_defaults()
+		.enable_metadata_hash("AJUN", 12)
+		.build()
+}
+
+#[cfg(not(feature = "std"))]
+fn main() {}
