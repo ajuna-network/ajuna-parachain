49df746d19d72254f6c4c9f5d2edc79e93de4e69
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-05-23 23:50:37 +0200

    v0.8.7 (bump proc macro)

--- runtime/ajuna/src/lib.rs
+++ runtime/ajuna/src/lib.rs
@@ -230,7 +230,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("ajuna"),
 	impl_name: create_runtime_str!("ajuna"),
 	authoring_version: 1,
-	spec_version: 806,
+	spec_version: 807,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
