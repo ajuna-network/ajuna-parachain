1ee96a937942dd1240426e5a40d95509f1e72c37
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-05-23 21:44:16 +0200

    Add polkadot-sdk patch

--- runtime/ajuna/src/lib.rs
+++ runtime/ajuna/src/lib.rs
@@ -230,7 +230,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("ajuna"),
 	impl_name: create_runtime_str!("ajuna"),
 	authoring_version: 1,
-	spec_version: 805,
+	spec_version: 806,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
