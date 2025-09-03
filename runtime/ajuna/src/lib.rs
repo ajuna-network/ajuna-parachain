3cd0c8ec39d7cf5d56a2f0ca16e9c3d332d8b8b8
clangenb <37865735+clangenb@users.noreply.github.com>
2025-09-03 10:20:43 +0200

    v0.8.8 (#97)

--- runtime/ajuna/src/lib.rs
+++ runtime/ajuna/src/lib.rs
@@ -249,7 +249,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("ajuna"),
 	impl_name: Cow::Borrowed("ajuna"),
 	authoring_version: 1,
-	spec_version: 807,
+	spec_version: 808,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
